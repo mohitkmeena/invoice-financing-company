065a15d445d490efdba499af7b3a944d81450805
mohitkmeena <mohitmeenaG2005@gmail.com>
2025-04-01 01:28:27 +0530

    company side initialized

--- README.md
+++
@@ -1,2 +0,0 @@
-# invoice-financing-company
-this is the company side for the invoice financing
---
+++ src/main/java/com/mohit/Invoice_financing_company/InvoiceFinancingCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.Invoice_financing_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceFinancingCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceFinancingCompanyApplication.class, args);
+	}
+
+}
